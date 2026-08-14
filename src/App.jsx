7194b507f7baf5086012e7725363f06b5135b0a4
Bharathi-pente <bharathi@penteai.com>
2026-08-14 13:36:44 +0530

    test pr pipeline

--- src/App.jsx
+++ src/App.jsx
@@ -92,6 +92,7 @@ export default function App() {
           Add / View / Edit / Delete products via a GraphQL API
         </p>
         <p>HEllo World!</p>
+        <p>23 years old</p>
       </header>
 
       {error && <div className="error">⚠ {error}</div>}
